0b943b037ae9baa425f4b122b63df25376447d87
govindkushwaha <53311992+govindkushwaha@users.noreply.github.com>
2021-12-28 23:03:26 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import React, { useState } from 'react'
 import Filter from './Component/Filter/Filter';
 import Header from './Component/Header/Header';
 import ProductList from './Component/ProductList/ProductList';
+import dummyData from './StaticFiles/data';
+
 
 function App() {
   const [categoryFilter, setCategoryFilter] = useState([]);
   const [mealFilter, setMealFilter] = useState([]);
   const [dietaryFilter, setDietaryFilter] = useState([]);
+  const [productData, setProductData] = useState(dummyData);
+  const [flagForSearch, setFlagForSearch] = useState(false);
 
   const onDietaryFilterChange = (values) => {
     let filterLists;
@@ -41,13 +45,14 @@ function App() {
   return (
     <>
       <div className="appbody">
-        <Header />
+        <Header setProductData= {setProductData} setFlagForSearch={setFlagForSearch}/>
         <main className='main'>
           <Filter categoryFilter={categoryFilter} onCategoryFilterChange={onCategoryFilterChange}
             mealFilter={mealFilter} onMealFilterChange={onMealFilterChange}
             dietaryFilter={dietaryFilter} onDietaryFilterChange={onDietaryFilterChange}
           />
-          <ProductList categoryFilter={categoryFilter} mealFilter={mealFilter} dietaryFilter={dietaryFilter} />
+          <ProductList categoryFilter={categoryFilter} mealFilter={mealFilter} dietaryFilter={dietaryFilter} 
+          productData={productData} flagForSearch={flagForSearch}/>
         </main>
 
       </div>
